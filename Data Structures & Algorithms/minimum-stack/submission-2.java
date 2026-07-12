5b91a03e1b1528f8b1c7f19f32ef5994eda8bf57
Gengar1990 <flugergeheimer21@gmail.com>
2026-07-12 15:21:27 +0300

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,30 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if (stack.isEmpty()) 
+            minStack.push(val);
+        else minStack.push(Math.min(minStack.peek(), val));
+        stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
